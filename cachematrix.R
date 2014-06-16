3246d08788497593d079a1b14ea321386be7a3e8
Wagner Spirigoni <wagnerspi@gmail.com>
2014-06-15 23:23:18 -0300

    Creating functions to store a cached version of a inversed matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## A function that implements 4 functions (set, get, setMatrix and getMatrix)
+## that can be used to store the result of a inversed matrix that has been set
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setMatrix <- function(matrix) m <<- matrix
+    getMatrix <- function() m
+    list(set = set, get = get,
+         setMatrix = setMatrix,
+         getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function checks if there is a cached version of the inversed matrix and
+## shows it without re-inversing, otherwise inverses the matrix, and store in
+## the makeCacheMatrix$setMatrix function, so the next time, it it's the same
+## matrix, it will return from the cached version.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getMatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setMatrix(m)
+    m
 }
